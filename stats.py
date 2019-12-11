2a3d09cf669530f7a75e87c3aa650f22a9778a36
Paul Lerner <1.paul.lerner@gmail.com>
2019-12-11 12:06:56 +0100

    feat: print speech duration quartiles

--- stats.py
+++ stats.py
@@ -8,6 +8,7 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("whitegrid", {'axes.grid' : False})
+import numpy as np
 
 from pyannote.database import get_protocol
 
@@ -20,6 +21,7 @@ protocol_str='{}.{}.{}'.format(DATABASE,TASK,PROTOCOL)
 
 def plot_speech_duration(stats):
     sns.distplot(list(stats['labels'].values()),kde=False,norm_hist=True)
+    plt.figure(figsize=(12,10))
     plt.ylabel("density")
     plt.xlabel("speech duration")
     plt.title(f"Normed histogram of the speech duration in {protocol_str}.{SET}")
@@ -28,10 +30,14 @@ def plot_speech_duration(stats):
 if __name__=='__main__':
 
     protocol = get_protocol(protocol_str)
-    print(f"gettings stats from {protocol_str}...")
+    print(f"gettings stats from {protocol_str}.{SET}...")
     stats=protocol.stats(SET)
     for key,value in stats.items():
         if key=='labels':
             break
         print(key,value)
+
+    print("speech duration quartiles :")
+    print(np.quantile(list(stats['labels'].values(),[0.,0.25,0.5,0.75,1.0]))
+
     plot_speech_duration(stats)
