1f786c94e3c92a7b6b9cf0e4a2d8b8f2d66d5551
Paul Lerner <1.paul.lerner@gmail.com>
2019-12-13 15:12:37 +0100

    feat: add docopt usage, pass some stuff as arguments

--- stats.py
+++ stats.py
@@ -2,9 +2,17 @@
 # encoding: utf-8
 """
 Gets stats and plots stuff given a protocol
+
+Usage:
+  stats.py <database.task.protocol> [--set=<set> --filter_unk --crop=<crop> --hist]
+  stats.py -h | --help
+
+Common options:
+  <database.task.protocol>   Experimental protocol (e.g. "Etape.SpeakerDiarization.TV")
 """
 
 import os
+from docopt import docopt
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("whitegrid", {'axes.grid' : False})
@@ -13,15 +21,7 @@ np.set_printoptions(precision=2, suppress=True)
 
 from pyannote.database import get_protocol
 
-DATABASE="Plumcot-Friends"
-TASK="SpeakerDiarization"
-PROTOCOL="UEM"
-SET='train'
-FIGURE_DIR='/people/lerner/Images'
-protocol_str='{}.{}.{}'.format(DATABASE,TASK,PROTOCOL)
-filter_unk=False
-crop=None
-hist=False
+
 
 def plot_speech_duration(values,hist=True,crop=None):
     keep_n=len(values) if crop is None else int(len(values)*crop)
@@ -29,27 +29,38 @@ def plot_speech_duration(values,hist=True,crop=None):
     values=values[-keep_n:]
     plt.figure(figsize=(12,10))
     title=(
-        f"of the speech duration in {protocol_str}.{SET} "
+        f"of the speech duration in {protocol_name}.{set} "
         f"of the {keep_n} biggest speakers"
     )
     if hist:
         sns.distplot(values,kde=False,norm_hist=True)
         plt.ylabel("density")
-        plt.xlabel("speech duration")
+        plt.xlabel("speech duration (s)")
         plt.title("Normed histogram "+title)
     else:
         plt.title("Plot "+title)
-        plt.ylabel("speech duration")
+        plt.ylabel("speech duration (s)")
         plt.xlabel("speaker #")
         plt.plot(values,".")
     fig_type="hist" if hist else "plot"
-    plt.savefig(os.path.join(FIGURE_DIR,f"speech_duration.{protocol_str}.{SET}.{fig_type}.{keep_n}.png"))
+    save_path=os.path.join(FIGURE_DIR,f"speech_duration.{protocol_name}.{set}.{fig_type}.{keep_n}.png")
+    plt.savefig(save_path)
+    print(f"succesfully saved {save_path}")
 
 if __name__=='__main__':
+    FIGURE_DIR='/people/lerner/Images'
 
-    protocol = get_protocol(protocol_str)
-    print(f"gettings stats from {protocol_str}.{SET}...")
-    stats=protocol.stats(SET)
+    args = docopt(__doc__)
+
+    protocol_name = args['<database.task.protocol>']
+    set=args['--set'] if args['--set'] else "train"
+    filter_unk=args['--filter_unk']
+    crop=float(args['--crop']) if args['--crop'] else None
+    hist=args['--hist']
+    
+    protocol = get_protocol(protocol_name)
+    print(f"gettings stats from {protocol_name}.{set}...")
+    stats=protocol.stats(set)
     for key,value in stats.items():
         if key=='labels':
             break
@@ -67,4 +78,5 @@ if __name__=='__main__':
     print("deciles:")
     print(np.quantile(values,np.arange(0,1.1,0.1)))
 
-    plot_speech_duration(values,hist,crop)
+    plot_speech_duration(values,hist,crop,
+        protocol_name, set)
