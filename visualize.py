e4db844352bb94f984b1f423d692913d65a445be
Paul Lerner <1.paul.lerner@gmail.com>
2020-01-30 19:27:09 +0100

    feat: visualize identification distances

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 """Usage:
   visualize.py gecko <hypotheses_path> <uri> [--protocol=<database.task.protocol>]
+  visualize.py distances <hypotheses_path> <uri> <database.task.protocol>
   visualize.py stats <database.task.protocol> [--set=<set> --filter_unk --crop=<crop> --hist --verbose]
   visualize.py -h | --help
 
@@ -18,13 +19,76 @@ import os
 from pathlib import Path
 import json
 from docopt import docopt
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid", {'axes.grid' : False})
+
+import re
+import numpy as np
+
 from pyannote.core import Annotation
 from pyannote.database.util import load_rttm, load_id
 from convert import annotation_to_GeckoJSON
-from pyannote.database import get_protocol
+from pyannote.database import get_protocol,get_annotated
 from pyannote.metrics.diarization import DiarizationErrorRate
 
 DATA_PATH=Path('/vol', 'work', 'lerner', 'pyannote-db-plumcot', 'Plumcot', 'data')
+from convert import DISTANCE_THRESHOLD
+
+def distances(args):
+    hypotheses_path=args['<hypotheses_path>']
+    hypotheses, distances=load_id(hypotheses_path)
+    uri=args['<uri>']
+    hypothesis, distances = hypotheses[uri], distances[uri]
+    protocol=args['<database.task.protocol>']
+    protocol = get_protocol(protocol)
+    for reference in getattr(protocol, 'test')():
+        if reference['uri']==uri:
+            break
+    if reference['uri']!=uri:
+        raise ValueError(f"{uri} is not in {protocol}.test")
+    annotated=get_annotated(reference)
+    annotation=reference['annotation'].crop(annotated)
+    true_distances,false_distances=[],[]
+    tp,fp=0,0
+    close_duration,far_duration=[],[]
+    for segment, track, label in hypothesis.itertracks(yield_label=True):
+        distance=distances.get(segment)
+        distance=distance.get(label) if distance else None
+        distance= distance if distance !="<NA>" else None
+        annotation_segment=annotation.crop(segment)
+        if distance:
+            if distance < DISTANCE_THRESHOLD:
+                close_duration.append(segment.duration)
+            else:
+                far_duration.append(segment.duration)
+        if label in annotation_segment.labels():
+            true_distances.append(distance)
+            tp+=segment.duration
+        else:
+            false_distances.append(distance)
+            fp+=segment.duration
+    print("debug precision %:",tp/(tp+fp))
+
+    density=False
+    true_label=f"True Positives ({len(true_distances)} #, {tp:.0f} s)"
+    n, bins, patches=plt.hist(true_distances,30,density=density,alpha=0.5,label=true_label)
+    false_label=f"False Positives ({len(false_distances)} #, {fp:.0f} s)"
+    plt.hist(false_distances,bins,density=density,alpha=0.5, label=false_label)
+    plt.axvline(x=DISTANCE_THRESHOLD,color="black", linestyle='dashed')
+    close_text=f"{len(close_duration)} #, {sum(close_duration):.0f} s"
+    plt.text(DISTANCE_THRESHOLD-0.2, np.max(n)/2, close_text)
+    far_text=f"{len(far_duration)} #, {sum(far_duration):.0f} s"
+    plt.text(DISTANCE_THRESHOLD+0.2, np.max(n)/2, far_text)
+    plt.legend()
+    title="FA: distance between hypothesis and reference in identification pipeline\n"
+    #title+="without clustering"
+    title+="clustering @DER"
+    print(re.sub("\s","_",title))
+    plt.title(title)
+    plt.ylabel("number of segments")
+    plt.xlabel("angular distance")
+    plt.show()
 
 def gecko(args):
     hypotheses_path=args['<hypotheses_path>']
@@ -51,7 +115,8 @@ def gecko(args):
             if reference['uri']==uri:
                 break
         diarizationErrorRate=DiarizationErrorRate()
-        optimal_mapping=diarizationErrorRate.optimal_mapping(reference['annotation'], hypothesis,reference['annotated'])
+        annotated=get_annotated(reference)
+        optimal_mapping=diarizationErrorRate.optimal_mapping(reference['annotation'], hypothesis,annotated)
         hypothesis=hypothesis.rename_labels(mapping=optimal_mapping)
 
     gecko_json=annotation_to_GeckoJSON(hypothesis, distances, colors)
@@ -65,6 +130,8 @@ if __name__ == '__main__':
     args = docopt(__doc__)
     if args['gecko']:
         gecko(args)
+    if args['distances']:
+        distances(args)
     if args['stats']:
         from stats import main as stats
         stats(args)
