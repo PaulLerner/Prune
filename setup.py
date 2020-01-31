192a578467b10a3f442aa9391e17c5d83a2e30a6
paullerner <1.paul.lerner@gmail.com>
2020-01-31 11:49:55 +0100

    chore: move code in prune folder and add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+
+    name='Prune',
+    packages=find_packages(),
+    scripts=[
+        "prune/stats.py",
+        "prune/visualize.py"
+    ],
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author='Paul Lerner',
+    author_email='lerner@limsi.fr',
+    url='https://github.com/PaulLerner/Prune',
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering"
+    ],
+)
