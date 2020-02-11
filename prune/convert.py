3abaab93b05922288cf5883bd7be85c797c55d35
paullerner <1.paul.lerner@gmail.com>
2020-02-11 16:33:16 +0100

    feat: convert gecko-JSON to Annotation, must/cannot-link constraints remove gecko-NA-values

--- prune/convert.py
+++ prune/convert.py
@@ -2,11 +2,15 @@
 # encoding: utf-8
 
 from pyannote.core.utils.distance import pdist
+from pyannote.core import Annotation, Segment, Timeline
 from scipy.spatial.distance import squareform
 import numpy as np
 
 import json
+import re
+
 DISTANCE_THRESHOLD=0.5
+NA_VALUES={'','<NA>'}
 
 def get_embeddings_per_speaker(features, hypothesis):
     """
@@ -76,6 +80,53 @@ def update_labels(annotation, distances):
             if distance > DISTANCE_THRESHOLD:
                 annotation[segment,track]=f"?{label}"
     return annotation
+
+def gecko_JSON_to_Annotation(gecko_JSON, uri=None, modality='speaker'):
+    """
+    Parameters:
+    -----------
+    gecko_JSON : `dict`
+        loaded from a Gecko-compliant JSON as defined in xml_to_GeckoJSON
+    uri (uniform resource identifier) : `str`
+        which identifies the annotation (e.g. episode number)
+        Default : None
+    modality : `str`
+        modality of the annotation as defined in https://github.com/pyannote/pyannote-core
+
+    Returns:
+    --------
+    annotation: pyannote `Annotation`
+        for speaker identification/diarization as defined in https://github.com/pyannote/pyannote-core
+    must_link: pyannote `Annotation`
+        User annotation
+    cannot_link: pyannote `Annotation`
+        User annotation of parts were the labelled speakers **do not** speak
+    annotated: pyannote `Timeline`
+        representing the annotated parts of the gecko_JSON files (depends on confidence_threshold)
+    """
+
+    annotation = Annotation(uri, modality)
+    must_link = Annotation(uri, modality)
+    cannot_link = Annotation(uri, f"non-{modality}")
+
+    for monologue in gecko_JSON["monologues"]:
+        segment = Segment(monologue["start"],monologue["end"])
+        # '@' defined in https://github.com/hbredin/pyannote-db-plumcot/blob/develop/CONTRIBUTING.md#idepisodetxt
+        # '+' defined in https://github.com/gong-io/gecko/blob/master/app/geckoModule/constants.js#L35
+        speaker_ids=re.split("@|\+",monologue["speaker"]["id"])
+        speaker_ids=set(speaker_ids)-NA_VALUES
+        for speaker_id in speaker_ids:#most of the time there's only one
+            annotation[segment,speaker_id] = speaker_id
+            if monologue["speaker"]["annotators"] > 0:
+                must_link[segment,speaker_id] = speaker_id
+
+        non_ids = monologue["speaker"]["non_id"]
+        non_ids=set(speaker_ids)-NA_VALUES
+        for speaker_id in non_ids:
+            cannot_link[segment,speaker_id] = speaker_id
+
+    annotated=Timeline([Segment(0.0,monologue["end"])],uri)
+    return annotation, must_link, cannot_link, annotated
 
 def annotation_to_GeckoJSON(annotation, distances, colors={}):
     """
