2678278397e95a3d1bbd451e3ab70cdc74b810f6
Paul Lerner <lerner@tuta.io>
2020-07-07 17:09:46 +0200

    refactor: use torch.prod instead of np.prod -> remove np import

--- prune/sidnet.py
+++ prune/sidnet.py
@@ -3,12 +3,10 @@
 
 from typing import List
 
-import numpy as np
-
 from transformers import BertModel
 from torch.nn import Transformer, Module, Linear, LogSoftmax
 from torch.cuda import device_count
-from torch import device
+from torch import device, prod
 
 
 DEVICES = [device('cpu')] if device_count() == 0 else \
@@ -19,7 +17,7 @@ def total_params(module):
     """Beware to freeze the relevant parameters before computing this."""
     trainable, total = 0, 0
     for param in module.parameters():
-        size = np.prod(param.size())
+        size = prod(param.size())
         total += size
         if param.requires_grad:
             trainable += size
