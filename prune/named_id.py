fix: do some magic with torch indexing and broadcasting

prune/named_id.py
@@ -863,13 +863,14 @@ def batch_encode_multi(tokenizer, text_batch, target_batch, mask=True, mask_name
             if t == tokenizer.pad_token_id:
                 continue
             input_where = input_id == t
-            target_where = (target_id == t)#.nonzero().reshape(-1)
+            target_where = (target_id == t).nonzero().reshape(-1)
             # speaker name is not mentioned in input -> pad target
             if not input_where.any():
                 tgt_key_padding_mask[i, target_where] = tokenizer.pad_token_id
                 continue
+            # FIXME: why is it necessary to do all this reshaping and indexing ?
             input_where = input_where.nonzero().reshape(-1)
-            relative_targets[i, target_where, input_where] = 1.
+            relative_targets[i, target_where][:, input_where] = 1.
             # mask target names in input
             ratio = np.random.rand()
             if ratio < mask_names: